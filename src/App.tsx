16dd728ba43d2e49186c2432e1b7a196f8d88888
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-03-03 13:30:24 -0800

    Changed files path, added footer and create ticket page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import Home from "@/pages/Home";
-import { Route, Routes } from "react-router";
-import LoginForm from "@/components/shared/auth/LoginForm";
-import SignupForm from "@/components/shared/auth/SignupForm";
-import Header from "@/components/shared/Header";
-import Protected from "@/components/shared/Protected";
+import Footer from "@/components/shared/Footer/Footer";
+import Header from "@/components/shared/Header/Header";
+import Protected from "@/components/shared/Protected/Protected";
 import { Toaster } from "@/components/ui/toaster";
 import { useUserContext } from "@/context/userContext";
 import { ROUTES } from "@/lib/constants/routes";
-import CreateTemplate from "@/pages/CreateTemplate";
-import Error from "@/pages/Error";
-import UseTemplate from "@/pages/UseTemplate";
-import Profile from "@/pages/Profile";
-import Settings from "@/pages/Settings";
-import TemplatePreview from "@/pages/TemplatePreview";
+import CreateTemplate from "@/pages/CreateTemplate/CreateTemplate";
+import CreateTicket from "@/pages/CreateTicket/CreateTicket";
+import Error from "@/pages/Error/Error";
+import Home from "@/pages/Home/Home";
+import Login from "@/pages/Login/Login";
+import Profile from "@/pages/Profile/Profile";
+import Settings from "@/pages/Settings/Settings";
+import Signup from "@/pages/Signup/Signup";
+import TemplatePreview from "@/pages/TemplatePreview/TemplatePreview";
+import UseTemplate from "@/pages/UseTemplate/UseTemplate";
+import { Route, Routes } from "react-router";
 
 function App() {
 	const { darkMode } = useUserContext();
@@ -23,9 +25,9 @@ function App() {
 			<Header />
 			<div className='flex flex-col flex-1 items-center dark:bg-zinc-900 p-8'>
 				<Routes>
-					<Route path={ROUTES.HOME} element={<Home />}></Route>
-					<Route path={ROUTES.SIGNUP} element={<SignupForm />} />
-					<Route path={ROUTES.LOGIN} element={<LoginForm />} />
+					<Route path={ROUTES.HOME} element={<Home />} />
+					<Route path={ROUTES.SIGNUP} element={<Signup />} />
+					<Route path={ROUTES.LOGIN} element={<Login />} />
 					<Route path={ROUTES.SETTINGS} element={<Settings />} />
 					<Route
 						path={`${ROUTES.TEMPLATE_PREVIEW}/:id`}
@@ -38,10 +40,12 @@ function App() {
 							path={`${ROUTES.USE_TEMPLATE}/:id`}
 							element={<UseTemplate />}
 						/>
+						<Route path={ROUTES.CREATE_TICKET} element={<CreateTicket />} />
 					</Route>
 					<Route path={ROUTES.ERROR} element={<Error />} />
 				</Routes>
 			</div>
+			<Footer />
 			<Toaster />
 		</main>
 	);
