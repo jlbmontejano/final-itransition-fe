c3f70d13af7bcba282e97012d8a392880a9a8764
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-03-05 12:08:09 -0800

    Added Ticket type

--- src/components/shared/TicketCard/TicketCard.tsx
+++ src/components/shared/TicketCard/TicketCard.tsx
@@ -1,55 +1,30 @@
 import {
 	Card,
-	CardContent,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { useUserContext } from "@/context/userContext";
-import { ROUTES } from "@/lib/constants/routes";
-import { useNavigate } from "react-router";
-import Likes from "../Likes/Likes";
-import Badges from "../Badges/Badges";
-import TEXT from "./utils/text";
+import { Ticket } from "@/types";
+import { Link } from "react-router";
 
-const TicketCard = ({ ticket }: { ticket: BasicTicketInfo }) => {
-	const { language } = useUserContext();
-	const navigate = useNavigate();
+const TicketCard = ({ summary, status, urlKey }: Ticket) => {
+	const urlConstructor = () =>
+		`${import.meta.env.VITE_TICKET_URL_FIRSTPART}
+	${urlKey}
+	${import.meta.env.VITE_TICKET_URL_SECONDPART}`;
 
 	return (
 		<Card className='w-full'>
 			<CardHeader>
-				<CardTitle
-					onClick={() => navigate(`${ROUTES.TEMPLATE_PREVIEW}/${template.id}`)}
-					className='text-xl hover:underline hover:cursor-pointer'>
-					{template.title}
+				<CardTitle className='text-xl hover:underline hover:cursor-pointer'>
+					<Link to={urlConstructor()} target='_blank'>
+						{summary}
+					</Link>
 				</CardTitle>
 				<CardDescription>
-					<p className='text-xs'>
-						{template.creator.name} | {template.topic.text}
-					</p>
+					<p className='text-xs'>Status: {status}</p>
 				</CardDescription>
 			</CardHeader>
-			<CardContent>
-				<p> {template.description}</p>
-			</CardContent>
-			<CardFooter className='flex gap-2 items-center justify-between'>
-				<div className='flex flex-wrap gap-1'>
-					{template.tags.length > 0 ? (
-						<Badges items={template.tags} />
-					) : (
-						<p className='text-xs text-muted-foreground'>
-							{TEXT[language].NO_TAGS}
-						</p>
-					)}
-				</div>
-				<Likes
-					templateId={template.id}
-					likedUsersIds={template.likedUsersIds}
-					initialLikesCount={template.likesCount}
-				/>
-			</CardFooter>
 		</Card>
 	);
 };
