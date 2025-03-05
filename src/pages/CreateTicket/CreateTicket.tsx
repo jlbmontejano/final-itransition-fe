e530c42ae30a5397c736b4fb94cc92b6172dad92
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-03-05 12:12:03 -0800

    Finished CreateTicket component

--- src/pages/CreateTicket/CreateTicket.tsx
+++ src/pages/CreateTicket/CreateTicket.tsx
@@ -7,7 +7,7 @@ import {
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { useUserContext } from "@/context/userContext";
 import { toast } from "@/hooks/use-toast";
 import { ROUTES } from "@/lib/constants/routes";
@@ -15,33 +15,45 @@ import { ticketSchema } from "@/lib/zod-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { z } from "zod";
 import TEXT from "./utils/text";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { PRIORITIES } from "./utils/constants";
+import { createTicket } from "@/lib/fetch";
+import Loader from "@/components/shared/Loader/Loader";
 
 const CreateTicket = () => {
 	const { user, language } = useUserContext();
+	const { state } = useLocation();
 	const navigate = useNavigate();
 	const [isLoading, setIsLoading] = useState(false);
 
 	const form = useForm<z.infer<typeof ticketSchema>>({
 		resolver: zodResolver(ticketSchema),
 		defaultValues: {
-			reportedBy: user?.email,
-			templateTitle: "",
-			link: "",
-			priority: "Low",
-			status: "In Progress",
+			email: user?.email,
+			link: state.location,
+			title: state.templateTitle || "",
+			summary: "",
+			priority: "Medium",
 		},
 	});
 
 	async function onSubmit(values: z.infer<typeof ticketSchema>) {
 		try {
-			console.log(values);
 			setIsLoading(true);
-			// const { data } = await createTicket(values);
+
+			await createTicket(values);
 
 			navigate(ROUTES.PROFILE);
+			toast({ description: TEXT[language].TOAST_SUCESS });
 		} catch (err) {
 			console.log(err);
 			toast({
@@ -54,20 +66,23 @@ const CreateTicket = () => {
 		}
 	}
 
+	if (isLoading) return <Loader />;
+
 	return (
-		<div>
+		<div className='w-full max-w-[500px]'>
+			<p className='page-title'>{TEXT[language].PAGE_TITLE}</p>
 			<Form {...form}>
 				<form
 					onSubmit={form.handleSubmit(onSubmit)}
 					className='space-y-8 dark:text-white'>
 					<FormField
 						control={form.control}
-						name='reportedBy'
+						name='summary'
 						render={({ field }) => (
 							<FormItem>
-								<FormLabel>{TEXT[language].REPORTED_BY}</FormLabel>
+								<FormLabel>{TEXT[language].SUMMARY}</FormLabel>
 								<FormControl>
-									<Input {...field} disabled={true} />
+									<Textarea {...field} />
 								</FormControl>
 								<FormMessage />
 							</FormItem>
@@ -75,13 +90,27 @@ const CreateTicket = () => {
 					/>
 					<FormField
 						control={form.control}
-						name='templateTitle'
+						name='priority'
 						render={({ field }) => (
 							<FormItem>
-								<FormLabel>{TEXT[language].TEMPLATE_TITLE}</FormLabel>
-								<FormControl>
-									<Input {...field} disabled={isLoading} />
-								</FormControl>
+								<FormLabel>{TEXT[language].PRIORITY}</FormLabel>
+								<Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+									{...field}>
+									<FormControl>
+										<SelectTrigger className='w-[180px]'>
+											<SelectValue />
+										</SelectTrigger>
+									</FormControl>
+									<SelectContent>
+										{PRIORITIES.map((priority, idx) => (
+											<SelectItem value={priority} key={`${priority}-${idx}`}>
+												{priority}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
 								<FormMessage />
 							</FormItem>
 						)}
