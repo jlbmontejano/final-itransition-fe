53509d8e59f71887ceba03132b0eba46e23cec42
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-03-05 12:06:44 -0800

    Added state to Link for Jira ticket creation

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -2,15 +2,23 @@ import { useUserContext } from "@/context/userContext";
 import TEXT from "./utils/text";
 import { Link } from "react-router";
 import { ROUTES } from "@/lib/constants/routes";
+import { useTemplateContext } from "@/context/templateContext";
 
 const Footer = () => {
 	const { language } = useUserContext();
+	const { currentTemplate } = useTemplateContext();
 
 	return (
 		<div className='footer'>
 			<p className='text-sm'>
 				<span className='font-semibold'>{TEXT[language].NEED_HELP} </span>
-				<Link to={ROUTES.CREATE_TICKET} className='hover:underline'>
+				<Link
+					to={ROUTES.CREATE_TICKET}
+					state={{
+						location: window.location.href,
+						templateTitle: currentTemplate?.title,
+					}}
+					className='hover:underline'>
 					{TEXT[language].CREATE_TICKET}
 				</Link>
 			</p>
